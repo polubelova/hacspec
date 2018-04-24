5a7778ff39458fbbb4e585e0bda0f5d3a5b95b70
Marina Polubelova <polubelovam@gmail.com>
2018-04-24 16:41:46 +0200

    fix zqpoly_mul

--- specs/kyber.py
+++ specs/kyber.py
@@ -8,13 +8,15 @@
 from speclib import *
 from hashlib import *
 
-zqelem = NewType('zqelem',int)
-zqpoly    = NewType('zqpoly',array[zqelem])
-zqpolyvec = NewType('zqpolyvec', array[zqpoly])
-
-
 kyber_q   = 7681
 kyber_n   =  256
+
+zqelem   = refine3(nat, lambda x: x < kyber_q)
+zqelem_t = zqelem
+
+zqpoly_t    = array[zqelem_t]
+zqpolyvec_t = array[zqpoly_t]
+
 kyber_dt  =   11
 kyber_du  =   11
 kyber_dv  =    3
@@ -22,48 +24,90 @@ kyber_dv  =    3
 kyber_k   =    3 # How do we want to handle different values?
 kyber_eta =    4 # How do we want to handle different values?
 
-
 omega     = zqelem(3844)
 psi       = zqelem(62)
 n_inv     = zqelem(7651)
 psi_inv   = zqelem(1115)
 omega_inv = zqelem(6584)
 
+def to_zqelem(x:nat_t) -> zqelem_t:
+    return zqelem(x % kyber_q)
 
-def zqadd(a:zqelem, b:zqelem) -> zqelem:
-    return zqelem(a+b % kyber_q)
+def zqadd(a:zqelem_t, b:zqelem_t) -> zqelem_t:
+    return to_zqelem(a + b)
 
-def zqsub(a:zqelem, b:zqelem) -> zqelem:
-    return zqelem((a+kyber_q-b) % kyber_q)
+def zqsub(a:zqelem_t, b:zqelem_t) -> zqelem_t:
+    return to_zqelem(a + kyber_q - b)
 
-def zqmul(a:zqelem, b:zqelem) -> zqelem:
-    return zqelem(a*b % kyber_q)
+def zqmul(a:zqelem_t, b:zqelem_t) -> zqelem_t:
+    return to_zqelem(a * b)
 
-def zqexp(a:zqelem, exp:int) -> zqelem:
-    return zqelem(pow(a,exp,kyber_q))
+def zqexp(a:zqelem_t, exp:nat_t) -> zqelem_t:
+    return to_zqelem(pow(a, exp, kyber_q))
 
-def zqpoly_ntt(p:zqpoly) -> zqpoly:
-    np = array.create(zqelem(0),256)
-    for i in range(256):
-        for j in range(256):
-            np[i] = zqadd(np[i],zqmul(zqmul(zqexp(psi,j),p[j]),zqexp(omega,(i*j))))
+def zqpoly_add(p:zqpoly_t, q:zqpoly_t) -> zqpoly_t:
+    np = array.create(kyber_n, zqelem(0))
+    for i in range(kyber_n):
+        np[i] = zqadd(p[i], q[i])
     return np
 
-def zqpoly_invntt(p:zqpoly) -> zqpoly:
-    np = array.create(zqelem(0),256)
-    for i in range(256):
-        for j in range(256):
-            np[i] = zqadd(np[i],zqmul(p[j],zqexp(omega_inv,(i*j))))
-        np[i] = zqmul(np[i],zqmul(n_inv,zqexp(psi_inv,i)))
+def zqpoly_sub(p:zqpoly_t, q:zqpoly_t) -> zqpoly_t:
+    np = array.create(kyber_n, zqelem(0))
+    for i in range(kyber_n):
+        np[i] = zqsub(p[i], q[i])
     return np
 
-def zqpoly_pointwise(p:zqpoly, q:zqpoly) -> zqpoly:
-    np = array.create(zqelem(0),256)
-    for i in range(256):
-        np[i] = zqmul(p[i],q[i])
+def zqpoly_pointwise_mul(p:zqpoly_t, q:zqpoly_t) -> zqpoly_t:
+    np = array.create(kyber_n, zqelem(0))
+    for i in range(kyber_n):
+        np[i] = zqmul(p[i], q[i])
     return np
 
+def zqpoly_ntt(p:zqpoly_t) -> zqpoly_t:
+    np = array.create(kyber_n, zqelem(0))
+    for i in range(kyber_n):
+        for j in range(kyber_n):
+            np[i] = zqadd(np[i], zqmul(zqmul(zqexp(psi, j), p[j]), zqexp(omega, i * j)))
+    return np
 
+def zqpoly_invntt(p:zqpoly_t) -> zqpoly_t:
+    np = array.create(kyber_n, zqelem(0))
+    for i in range(kyber_n):
+        for j in range(kyber_n):
+            np[i] = zqadd(np[i], zqmul(p[j], zqexp(omega_inv, i * j)))
+        np[i] = zqmul(np[i], zqmul(n_inv, zqexp(psi_inv, i)))
+    return np
+
+def zqpoly_mul(p:zqpoly_t, q:zqpoly_t) -> zqpoly_t:
+    return zqpoly_invntt(zqpoly_pointwise_mul(zqpoly_ntt(p), zqpoly_ntt(q)))
+
+# just for debugging
+def zqpoly_mul_schoolbook(p:zqpoly_t, q:zqpoly_t) -> zqpoly_t:
+    s = array.create(kyber_n + kyber_n, zqelem(0))
+    for i in range(kyber_n):
+        for j in range(kyber_n):
+            s[i+j] = zqadd(s[i+j], zqmul(p[i], q[j]))
+    high = s[kyber_n:(kyber_n + kyber_n)]
+    low = s[0:kyber_n]
+    r = zqpoly_sub(low, high)
+    return r
+
+a = array.create(256, zqelem(0))
+for i in range(256):
+    a[i] = zqelem(i)
+
+b = array.create(256, zqelem(0))
+for i in range(256):
+    b[i] = zqelem(i+42)
+
+r1 = zqpoly_mul_schoolbook(a, b)
+r2 = zqpoly_mul(a, b)
+
+print(r1)
+print(r2)
+print(r1 == r2)
+
+'''
 def kyber_cpapke_keypair() -> (bytes, bytes):
     d = randombytes(32)
     rhosigma = bytes(hashlib.sha3_512(d))
@@ -71,7 +115,7 @@ def kyber_cpapke_keypair() -> (bytes, bytes):
     for i in range(256):
         for j in range(256):
             A[i][j] = genAij(rhosigma[0:32],i,j)
-    
+
     for i in range(kyber_k):
         s[i] = cbd(prf(rhosigma[32:64],n))
         n += 1
@@ -86,7 +130,7 @@ def kyber_cpapke_keypair() -> (bytes, bytes):
     for i in range(kyber_k):
         for j in range(kyber_k):
             that[i] = zqpoly_add(that[i], zqpoly_pointwise(A[i][j],shat[j]))
-    
+
     t = zqpolyvec_invntt(that)
     t = zqpolyvec_add(t, e)
 
@@ -104,7 +148,7 @@ def kyber_cpapke_encrypt(pk:bytes, m:bytes, coins:bytes) -> bytes:
     for i in range(256):
         for j in range(256):
             At[i][j] = genAij(rho,j,i)
- 
+
     for i in range(kyber_k):
         r[i] = cbd(prf(coins,n))
         n += 1
@@ -116,7 +160,7 @@ def kyber_cpapke_encrypt(pk:bytes, m:bytes, coins:bytes) -> bytes:
     e2 = cbd(prf(coins,n))
 
     rhat = zqpolyvec_ntt(r)
-    
+
     for i in range(kyber_k):
         for j in range(kyber_k):
             uhat[i] = zqpoly_add(uhat[i], zqpoly_pointwise(At[i][j],rhat[j]))
@@ -145,43 +189,4 @@ def kyber_cpapke_decrypt(sk:bytes, c:bytes) -> bytes:
     d = zqpoly_sub(v,zqpoly_invntt(dhat))
 
     return frombytes(d)
-
-
-   
-
-
-    
- 
-
-# just for debugging
-def mul_schoolbook(p:zqpoly, q:zqpoly) -> zqpoly:
-    t = array.create(zqelem(0),511)
-    r = array.create(zqelem(0),256)
-    for i in range(256):
-        for j in range(256):
-            t[i+j] = zqadd(t[i+j], zqmul(a[i],b[j]))
-    for i in range(255):
-        r[i] = zqsub(t[i],t[i+256])
-    r[255] = t[255] % kyber_q
-    return r
-    
-def mul_nttbased(p:zqpoly, q:zqpoly) -> zqpoly:
-    return invntt(zqpoly_pointwise(ntt(p),ntt(q)))
-
-
-
-a = array.create(zqelem(0),256)
-for i in range(256):
-    a[i] = zqelem(i)
-    
-b = array.create(zqelem(0),256)
-for i in range(256):
-    b[i] = zqelem(i+42)
-
-r1 = mul_schoolbook(a,b)
-r2 = mul_nttbased(a,b)
-
-print(r1)
-print(r2)
-print(r1 == r2)
-
+'''
